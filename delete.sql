6678baaa661895197c3792189ab280ce59c5c850
Sebastián Estacio R <80277545+z0s3r77@users.noreply.github.com>
2023-01-27 17:55:14 +0100

    Create delete.sql

---
+++ delete.sql
@@ -0,0 +1,22 @@
+-- Borrar todo el contenido de las tablas, sin borrar las tablas;
+
+
+DELETE FROM facturajuegos;
+
+DELETE FROM facturaconsola;
+
+DELETE FROM juegoconsola;
+
+DELETE FROM tiendajuegos;
+
+DELETE FROM tiendaconsola;
+
+DELETE FROM factura;
+
+DELETE FROM juego;
+
+DELETE FROM consola;
+
+DELETE FROM tienda;
+
+DELETE FROM cliente;
